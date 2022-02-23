345d8f96bb2c7e5ce1e3ec8aaf9ba837e8a7afb4
muhammedulas <muhammedulas3333@gmail.com>
2022-02-23 11:13:47 +0300

    basic authentication service for test purposes.

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,1 +1,15 @@
-class AuthService {}
+import 'package:flutter/material.dart';
+
+class AuthService with ChangeNotifier {
+  bool loggedIn = false;
+
+  void login() {
+    loggedIn = true;
+    notifyListeners();
+  }
+
+  void logout() {
+    loggedIn = false;
+    notifyListeners();
+  }
+}
